e2446619105579fa3a4a4c3d9c96e98de9c174cd
Gabriel Gómez de la Torre <gabriel.gomezdelatorre@hotmail.com>
2024-05-09 16:43:39 -0500

    Update analyze.go

--- cmd/policy-assistant/pkg/cli/analyze.go
+++ cmd/policy-assistant/pkg/cli/analyze.go
@@ -224,7 +224,7 @@ func QueryTraffic(explainedPolicies *matcher.Policy, trafficPath string) {
 	utils.DoOrDie(err)
 
 	for _, traffic := range *allTraffics {
-		tmppr := json.Parse(traffic.Source.Translate())
+		tmppr := json.Parse[[]*matcher.TrafficPeer](traffic.Source.Translate())
 		fmt.Println(tmppr)
 		
 		fmt.Printf("Traffic:\n%s\n", traffic.Table())
