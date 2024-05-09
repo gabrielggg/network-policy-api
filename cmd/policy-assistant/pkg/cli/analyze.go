bb8bdd2b3aea4f083e9ec05d2f6453f7cbeb9700
Gabriel Gómez de la Torre <gabriel.gomezdelatorre@hotmail.com>
2024-05-09 16:29:50 -0500

    Update analyze.go

--- cmd/policy-assistant/pkg/cli/analyze.go
+++ cmd/policy-assistant/pkg/cli/analyze.go
@@ -224,7 +224,7 @@ func QueryTraffic(explainedPolicies *matcher.Policy, trafficPath string) {
 	utils.DoOrDie(err)
 
 	for _, traffic := range *allTraffics {
-		testpr := traffic.Translate()
+		testpr := traffic.Source.Translate()
 		fmt.Printf("Traffic:\n%s\n", traffic.Table())
 
 		result := explainedPolicies.IsTrafficAllowed(traffic)
