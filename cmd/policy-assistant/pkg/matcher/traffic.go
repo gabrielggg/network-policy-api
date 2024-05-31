6c80f3866c5d3c4e1d9044a225d191dd1eb072be
Gabriel Gómez de la Torre <gabriel.gomezdelatorre@hotmail.com>
2024-05-31 15:16:10 -0500

    Update traffic.go

--- cmd/policy-assistant/pkg/matcher/traffic.go
+++ cmd/policy-assistant/pkg/matcher/traffic.go
@@ -95,6 +95,7 @@ func (p *TrafficPeer) Translate() TrafficPeer {
 	kubeDaemonSets, err := kubeClient.GetDaemonSetsInNamespace(workloadMetadata[0])
 	//fmt.Println(kubeDaemonSets)
 	kubeNamespaces, err := kubeClient.GetAllNamespaces()
+	fmt.Println(kubeNamespaces)
 	if err != nil {
 		logrus.Fatalf("unable to read pods from kube, ns '%s': %+v", workloadMetadata[0], err)
 	}
